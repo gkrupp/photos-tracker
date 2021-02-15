519541758351e7a2ad5ffebb957b366fa8b8ea4d
gkrupp <kruppaigabor@gmail.com>
2021-02-15 22:52:16 +0200

    config

---
+++ config/index.js
@@ -0,0 +1,48 @@
+require('dotenv')
+const path = require('path')
+
+module.exports = {
+  commonPath: process.env.COMMON_PATH,
+  common: (file) => path.resolve(process.env.COMMON_PATH, file),
+  tracker: {
+    roots: process.env.TRACKER_ROOTS.split(',').map((root, i) => {
+      return {
+        path: root,
+        userId: process.env.TRACKER_USERS.split(',')[i]
+      }
+    }),
+    startup: Boolean(process.env.TRACKER_STARTUP) || false,
+    watch: Boolean(process.env.TRACKER_WATCH) || false
+  },
+  content: {
+    thumbDir: process.env.CONTENT_THUMB_DIR,
+    thumbTypes: process.env.CONTENT_THUMB_TYPES.split(',')
+  },
+  proc: {
+    host: process.env.PROC_HOST,
+    queuePrefix: process.env.PROC_QUEUE_PREFIX,
+    processes: Number(process.env.PROC_NUMBER),
+    logLevel: process.env.PROC_LOG_LEVEL
+  },
+  queues: {
+    tracker: process.env.COMMON_Q_TRACKER,
+    processor: process.env.COMMON_Q_PROCESSOR
+  },
+  redis: {
+    host: process.env.RD_HOST,
+    password: process.env.RD_PWD
+  },
+  mongo: {
+    uri: process.env.MONGO_URI,
+    db: process.env.MONGO_DB,
+    collections: {
+      users: process.env.MONGO_COLL_USERS,
+      albums: process.env.MONGO_COLL_ALBUMS,
+      photos: process.env.MONGO_COLL_PHOTOS
+    },
+    options: {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  }
+}
